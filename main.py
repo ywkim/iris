4d2595d479e0c119575123a219be5d6236be133e
Youngwook Kim <youngwook.kim@gmail.com>
2023-07-02 08:54:42 +0900

    feat: Add speech recognition functionality
    This reverts commit d4f30f7ef88359fa9b744e1dcb9128c9e8c8b491.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 import openai
 import pvporcupine
+import speech_recognition as sr
 from pvrecorder import PvRecorder
 
 access_key = "YOUR_ACCESS_KEY"
@@ -72,92 +73,17 @@ def transcribe():
         except openai.error.ServiceUnavailableError:
             pass
 
-def listen():
-    import webrtcvad
-    import pyaudio
-    import collections
-    import struct
-    import wave
-
-    # Set sample rate and frame duration
-    SAMPLE_RATE = 16000
-    N_CHANNELS = 1
-    SAMPLE_WIDTH = 2  # 16 bit
-    CHUNK_DURATION_MS = 30  # 10, 20 or 30
-    CHUNK_SIZE = int(SAMPLE_RATE * CHUNK_DURATION_MS / 1000)  # chunk to read
-    PADDING_DURATION_MS = 1000  # chunk to read
-    PADDING_SIZE = int(SAMPLE_RATE * PADDING_DURATION_MS / 1000)  # chunk to read
-
-    # Create a VAD object
-    vad = webrtcvad.Vad()
-
-    # Set aggressiveness mode, which is a integer between 0 and 3. 0 is the least aggressive about filtering out non-speech, 3 is the most aggressive.
-    vad.set_mode(3)
-
-    # Initialize the microphone
-    pa = pyaudio.PyAudio()
-    sample_format = pa.get_format_from_width(SAMPLE_WIDTH)
-    audio_stream = pa.open(
-        format=sample_format,
-        rate=SAMPLE_RATE,
-        channels=N_CHANNELS,
-        input=True,
-        frames_per_buffer=CHUNK_SIZE)
-
-    # Create a wave file to store the audio
-    output_path = "command.wav"
-    wav_file = wave.open(output_path, "wb")
-    wav_file.setnchannels(N_CHANNELS)
-    wav_file.setsampwidth(pa.get_sample_size(sample_format))
-    wav_file.setframerate(SAMPLE_RATE)
-
-    # Read audio until voice command ends
-    while True:
-        # Read a chunk of raw audio data from the microphone
-        pcm = audio_stream.read(CHUNK_SIZE)
-
-        # Convert raw data to int16 array
-        # n_samples_per_buffer = n_frames_per_buffer * N_CHANNELS
-        # in_data = struct.unpack("%dh" % n_samples_per_buffer, chunk)
-        # pcm = struct.unpack_from("h" * 1024, pcm)
-
-        # Use VAD to check if the current chunk contains speech
-        is_speech = vad.is_speech(pcm, SAMPLE_RATE)
-
-        # If the audio data contains speech, write it to the wave file
-        if is_speech:
-            print("Voice detected, start recording")
-            wav_file.writeframes(pcm)
-
-            # If speech is detected, continue reading audio chunks
-            # until no speech is detected
-            while True:
-                is_speech = False
-                for _ in range(PADDING_SIZE // CHUNK_SIZE):
-                    pcm = audio_stream.read(CHUNK_SIZE)
-                    wav_file.writeframes(pcm)
-                    if vad.is_speech(pcm, SAMPLE_RATE):
-                        print("Speech detected")
-                        is_speech = True
-                    else:
-                        print("Speech not detected")
-                if not is_speech:
-                    print("Speech ended")
-                    break
-            break
-        else:
-            print("No speech detected")
-
-    # Stop the recording
-    audio_stream.stop_stream()
-    audio_stream.close()
-    pa.terminate()
-    wav_file.close()
-
-    transcribe()
-
 
 try:
+    # Initialize the recognizer
+    r = sr.Recognizer()
+
+    # Adjust the noise level
+    with sr.Microphone() as source:
+        print("Adjusting for noise...")
+        r.adjust_for_ambient_noise(source)
+        print("Done")
+
     while True:
         pcm = recorder.read()
         result = porcupine.process(pcm)
@@ -165,7 +91,20 @@ try:
         if result >= 0:
             print("[%s] Detected %s" % (str(datetime.now()), keywords[result]))
 
-            listen()
+            import speech_recognition as sr
+
+            # Open the microphone as a source
+            with sr.Microphone() as source:
+                # Listen for a voice command until it ends
+                print("Listening...")
+                audio = r.listen(source, timeout=5, phrase_time_limit=10)
+                print("Finished listening")
+
+            # 오디오를 WAV 파일로 저장
+            with open("command.wav", "wb") as out:
+                out.write(audio.get_wav_data())
+
+            transcribe()
         else:
             print("Not detected")
 
